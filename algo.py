29f0f87f6945d541f48e51bd8bc0e581fad09f9a
Himanshu Prasad <chatwith.himanshu27@gmail.com>
2021-12-19 01:00:55 +0530

    updated files

--- algo.py
+++ algo.py
@@ -1,13 +1,8 @@
 import json
+import re
 
-# file = open('input.json')
-# input = json.loads(file)
-
-# for i in input['sms']:
-#     print(i)
-
-# file.close()
-file_path = 'C:\Users\Himanshu J Prasad\Desktop\Gullak\input.json'
-with open('C:\Users\Himanshu J Prasad\Desktop\Gullak\input.json','r') as f:
+file_path = 'output.json'
+with open(file_path,'r') as f:
     data = json.loads(f.read())
-    print(data[0]['text'])
+    print(data)
+
