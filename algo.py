cf483bcb86b87782fc774b27969b7efa6b0d1cb6
Himanshu Prasad <chatwith.himanshu27@gmail.com>
2021-12-19 15:02:00 +0530

    working algo file

--- algo.py
+++ algo.py
@@ -2,37 +2,13 @@ import json
 import re
 
 categoryMapping = {'upiswiggy':"Food","BBPSBP":"Bill Payment"}
+
 def toCategorize(text):
-    #obj = {}
     category = ""
-    #smsArray = []
-    totalAmount = 0
-    if(re.search("[a-z]+\s@[a-z]+",text)):
-        category="Food"
-        val = "".join(extractAmount(text))
-        totalAmount += float(val)
-        # time = item['time']
-        # sender = item['sender']
-        # text = item['text']
-        #newbalance = item['New balance']
-        # smsArray.append({"time":time,"sender":sender,"text":text,"New balance":newbalance})
-        # obj["category"]=category
-        # obj["totalAmount"]=totalAmount
-        # obj["smsArray"] = smsArray
-    elif(re.search("[A-Z]+",text)): 
-        totalAmount = 0
-        category="Bill Payment"
-        val = "".join(extractAmount(text))
-        totalAmount += float(val)
-        # time = item['time']
-        # sender = item['sender']
-        # text = item['text']
-        #newbalance = item['New balance']
-        # smsArray.append({"time":time,"sender":sender,"text":text,"New balance":newbalance})
-        # obj["category"]=category
-        # obj["totalAmount"]=totalAmount
-        # obj["smsArray"] = smsArray
-    return totalAmount,category
+    textToSearch = text.split(' ')[11]
+    val = "".join(extractAmount(text))
+    category = categoryMapping[textToSearch]
+    return val,category
     
 def extractAmount(text):
     amount = 0
@@ -55,10 +31,10 @@ if __name__== "__main__":
         # get index from summary where category is smscategory
         if any(obj['category'] == category for obj in summary):
             tmpObj = [ele for ele in summary if ele['category']==category][0]
-            tmpObj['totalAmount']+=amount
+            tmpObj['totalAmount']+=float(amount)
             tmpObj['smsArray'].append(sms)
         else:
-            summary.append({'category':category,'totalAmount':amount,'smsArray':[sms]})    
+            summary.append({'category':category,'totalAmount':float(amount),'smsArray':[sms]})    
     output["summary"] = summary
         
 json_object = json.dumps(output,indent = 4)
