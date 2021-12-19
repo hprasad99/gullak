871f133c1aa8e639926d1251d12390c0976b00d5
Himanshu Prasad <chatwith.himanshu27@gmail.com>
2021-12-19 14:49:32 +0530

    working algo

--- algo.py
+++ algo.py
@@ -1,37 +1,38 @@
 import json
 import re
 
-def toCategorize(text,item):
-    obj = {}
+categoryMapping = {'upiswiggy':"Food","BBPSBP":"Bill Payment"}
+def toCategorize(text):
+    #obj = {}
     category = ""
-    smsArray = []
+    #smsArray = []
     totalAmount = 0
     if(re.search("[a-z]+\s@[a-z]+",text)):
         category="Food"
         val = "".join(extractAmount(text))
         totalAmount += float(val)
-        time = item['time']
-        sender = item['sender']
-        text = item['text']
-        newbalance = item['New balance']
-        smsArray.append({"time":time,"sender":sender,"text":text,"New balance":newbalance})
-        obj["category"]=category
-        obj["totalAmount"]=totalAmount
-        obj["smsArray"] = smsArray
+        # time = item['time']
+        # sender = item['sender']
+        # text = item['text']
+        #newbalance = item['New balance']
+        # smsArray.append({"time":time,"sender":sender,"text":text,"New balance":newbalance})
+        # obj["category"]=category
+        # obj["totalAmount"]=totalAmount
+        # obj["smsArray"] = smsArray
     elif(re.search("[A-Z]+",text)): 
         totalAmount = 0
         category="Bill Payment"
         val = "".join(extractAmount(text))
         totalAmount += float(val)
-        time = item['time']
-        sender = item['sender']
-        text = item['text']
-        newbalance = item['New balance']
-        smsArray.append({"time":time,"sender":sender,"text":text,"New balance":newbalance})
-        obj["category"]=category
-        obj["totalAmount"]=totalAmount
-        obj["smsArray"] = smsArray
-    return obj
+        # time = item['time']
+        # sender = item['sender']
+        # text = item['text']
+        #newbalance = item['New balance']
+        # smsArray.append({"time":time,"sender":sender,"text":text,"New balance":newbalance})
+        # obj["category"]=category
+        # obj["totalAmount"]=totalAmount
+        # obj["smsArray"] = smsArray
+    return totalAmount,category
     
 def extractAmount(text):
     amount = 0
@@ -42,17 +43,25 @@ def extractAmount(text):
 if __name__== "__main__":
     file_path = 'input.json'
     summary = []
-    main = {}
+    output = {}
     with open(file_path,'r') as f:
         data = json.loads(f.read())
     
-    for item in data['sms']:
-        text = item['text'];
+    for sms in data['sms']:
+        text = sms['text'];
         # extracted text to be categorized
-        summary.append(toCategorize(text,item))
-        main["summary"] = summary
+        #summary.append(toCategorize(text,item))
+        amount,category = toCategorize(text)
+        # get index from summary where category is smscategory
+        if any(obj['category'] == category for obj in summary):
+            tmpObj = [ele for ele in summary if ele['category']==category][0]
+            tmpObj['totalAmount']+=amount
+            tmpObj['smsArray'].append(sms)
+        else:
+            summary.append({'category':category,'totalAmount':amount,'smsArray':[sms]})    
+    output["summary"] = summary
         
-json_object = json.dumps(main,indent = 4)
+json_object = json.dumps(output,indent = 4)
         
 #writing to out.json file
 with open("out.json","w") as outfile:
